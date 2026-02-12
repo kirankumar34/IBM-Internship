e9de76f1597f674d8b52b67f26082c5b9a641eac
Kiran Kumar <kiransmart00@gmail.com>
2026-02-12 15:14:43 +0530

    deploying

--- frontend/src/context/api.js
+++ frontend/src/context/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "/api",
+    baseURL: "https://internship-9hwf.onrender.com",
 });
 
 // Request interceptor to add token
